9890f152bb099a779a429d1f42b0e5f03441dd36
nilsostertag <nostertag@gmx.de>
2025-04-29 23:58:27 +0200

    switched to csv data format

--- src/generateTrainingDataXOR.cpp
+++ src/generateTrainingDataXOR.cpp
@@ -3,12 +3,11 @@
 #include <cstdlib>
 
 int main() {
-    std::cout << "topology: 2 4 1" << std::endl;
+    std::cout << "n1,n2,target" << std::endl;
     for(int i = 2000; i >= 0; --i) {
         int n1 = (int)(2.0 * rand() / double(RAND_MAX));
         int n2 = (int)(2.0 * rand() / double(RAND_MAX));
         int t = n1 ^ n2;
-        std::cout << "in: " << n1 << ".0 " << n2 << ".0" << std::endl;
-        std::cout << "out: " << t << ".0" << std::endl;
+        std::cout << n1 << ".0," << n2 << ".0," << t << ".0" << std::endl;
     }
 }
